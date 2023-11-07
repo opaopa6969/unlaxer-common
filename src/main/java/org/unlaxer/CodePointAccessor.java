66fbbdcec709951b4fdb6404fad684ce9f5c7e2f
opa <opaopa6969@gmail.com>
2023-11-07 22:04:41 +0900

    add indexOf variations

--- src/main/java/org/unlaxer/CodePointAccessor.java
+++ src/main/java/org/unlaxer/CodePointAccessor.java
@@ -1,5 +1,7 @@
 package org.unlaxer;
 
+import java.util.Optional;
+
 public interface CodePointAccessor extends Comparable<CodePointAccessor>, StringBase {
   
   
@@ -99,11 +101,11 @@ public interface CodePointAccessor extends Comparable<CodePointAccessor>, String
     return endsWith(suffix.source());
   }
 
-  default CodePointIndexWithNegativeValue indexOf(CodePoint codePoint) {
-    return new CodePointIndexWithNegativeValue(
-        toCodePointIndex(new StringIndex(
-            indexOf(codePoint.value()))));
-  }
+//  default CodePointIndexWithNegativeValue indexOf(CodePoint codePoint) {
+//    return new CodePointIndexWithNegativeValue(
+//        toCodePointIndex(new StringIndex(
+//            indexOf(codePoint.value()))));
+//  }
   
   default CodePointIndexWithNegativeValue indexOf(CodePoint codePoint, CodePointIndex fromIndex) {
     return new CodePointIndexWithNegativeValue(
@@ -180,4 +182,296 @@ public interface CodePointAccessor extends Comparable<CodePointAccessor>, String
   }
   
   int[] subCodePoints(CodePointIndex startIndexInclusive, CodePointIndex endIndexExclusive);
+  
+  
+  /**
+   * Returns the index within this string of the first occurrence of
+   * the specified character. If a character with value
+   * {@code ch} occurs in the character sequence represented by
+   * this {@code String} object, then the index (in Unicode
+   * code units) of the first such occurrence is returned. For
+   * values of {@code ch} in the range from 0 to 0xFFFF
+   * (inclusive), this is the smallest value <i>k</i> such that:
+   * <blockquote><pre>
+   * this.charAt(<i>k</i>) == ch
+   * </pre></blockquote>
+   * is true. For other values of {@code ch}, it is the
+   * smallest value <i>k</i> such that:
+   * <blockquote><pre>
+   * this.codePointAt(<i>k</i>) == ch
+   * </pre></blockquote>
+   * is true. In either case, if no such character occurs in this
+   * string, then {@code -1} is returned.
+   *
+   * @param   ch   a character (Unicode code point).
+   * @return  the index of the first occurrence of the character in the
+   *          character sequence represented by this object, or
+   *          {@code -1} if the character does not occur.
+   */
+  default Optional<CodePointIndex> codePointIndexOf(int ch) {
+    int indexOf = indexOf(ch);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointIndexOf(CodePoint ch) {
+    return codePointIndexOf(ch.value());
+  }
+  
+  /**
+   * Returns the index within this string of the first occurrence of the
+   * specified character, starting the search at the specified index.
+   * <p>
+   * If a character with value {@code ch} occurs in the
+   * character sequence represented by this {@code String}
+   * object at an index no smaller than {@code fromIndex}, then
+   * the index of the first such occurrence is returned. For values
+   * of {@code ch} in the range from 0 to 0xFFFF (inclusive),
+   * this is the smallest value <i>k</i> such that:
+   * <blockquote><pre>
+   * (this.charAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &gt;= fromIndex)
+   * </pre></blockquote>
+   * is true. For other values of {@code ch}, it is the
+   * smallest value <i>k</i> such that:
+   * <blockquote><pre>
+   * (this.codePointAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &gt;= fromIndex)
+   * </pre></blockquote>
+   * is true. In either case, if no such character occurs in this
+   * string at or after position {@code fromIndex}, then
+   * {@code -1} is returned.
+   *
+   * <p>
+   * There is no restriction on the value of {@code fromIndex}. If it
+   * is negative, it has the same effect as if it were zero: this entire
+   * string may be searched. If it is greater than the length of this
+   * string, it has the same effect as if it were equal to the length of
+   * this string: {@code -1} is returned.
+   *
+   * <p>All indices are specified in {@code char} values
+   * (Unicode code units).
+   *
+   * @param   ch          a character (Unicode code point).
+   * @param   fromIndex   the index to start the search from.
+   * @return  the index of the first occurrence of the character in the
+   *          character sequence represented by this object that is greater
+   *          than or equal to {@code fromIndex}, or {@code -1}
+   *          if the character does not occur.
+   */
+  default Optional<CodePointIndex> codePointIndexOf(int ch, int fromIndex) {
+    int indexOf = indexOf(ch,fromIndex);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointIndexOf(CodePoint ch, CodePointIndex fromIndex) {
+    return codePointIndexOf(ch.value(), fromIndex.value());
+  }
+
+  
+  /**
+   * Returns the index within this string of the last occurrence of
+   * the specified character. For values of {@code ch} in the
+   * range from 0 to 0xFFFF (inclusive), the index (in Unicode code
+   * units) returned is the largest value <i>k</i> such that:
+   * <blockquote><pre>
+   * this.charAt(<i>k</i>) == ch
+   * </pre></blockquote>
+   * is true. For other values of {@code ch}, it is the
+   * largest value <i>k</i> such that:
+   * <blockquote><pre>
+   * this.codePointAt(<i>k</i>) == ch
+   * </pre></blockquote>
+   * is true.  In either case, if no such character occurs in this
+   * string, then {@code -1} is returned.  The
+   * {@code String} is searched backwards starting at the last
+   * character.
+   *
+   * @param   ch   a character (Unicode code point).
+   * @return  the index of the last occurrence of the character in the
+   *          character sequence represented by this object, or
+   *          {@code -1} if the character does not occur.
+   */
+  default Optional<CodePointIndex> codePointLastIndexOf(int ch) {
+    int indexOf = lastIndexOf(ch);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointLastIndexOf(CodePoint ch) {
+    return codePointLastIndexOf(ch.value());
+  }
+  
+  /**
+   * Returns the index within this string of the last occurrence of
+   * the specified character, searching backward starting at the
+   * specified index. For values of {@code ch} in the range
+   * from 0 to 0xFFFF (inclusive), the index returned is the largest
+   * value <i>k</i> such that:
+   * <blockquote><pre>
+   * (this.charAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &lt;= fromIndex)
+   * </pre></blockquote>
+   * is true. For other values of {@code ch}, it is the
+   * largest value <i>k</i> such that:
+   * <blockquote><pre>
+   * (this.codePointAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &lt;= fromIndex)
+   * </pre></blockquote>
+   * is true. In either case, if no such character occurs in this
+   * string at or before position {@code fromIndex}, then
+   * {@code -1} is returned.
+   *
+   * <p>All indices are specified in {@code char} values
+   * (Unicode code units).
+   *
+   * @param   ch          a character (Unicode code point).
+   * @param   fromIndex   the index to start the search from. There is no
+   *          restriction on the value of {@code fromIndex}. If it is
+   *          greater than or equal to the length of this string, it has
+   *          the same effect as if it were equal to one less than the
+   *          length of this string: this entire string may be searched.
+   *          If it is negative, it has the same effect as if it were -1:
+   *          -1 is returned.
+   * @return  the index of the last occurrence of the character in the
+   *          character sequence represented by this object that is less
+   *          than or equal to {@code fromIndex}, or {@code -1}
+   *          if the character does not occur before that point.
+   */
+  default Optional<CodePointIndex> codePointLastIndexOf(int ch, int fromIndex) {
+    int indexOf = lastIndexOf(ch,fromIndex);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointLastIndexOf(CodePoint ch, CodePointIndex fromIndex) {
+    return codePointLastIndexOf(ch.value(), fromIndex.value());
+  }
+
+  
+  /**
+   * Returns the index within this string of the first occurrence of the
+   * specified substring.
+   *
+   * <p>The returned index is the smallest value {@code k} for which:
+   * <pre>{@code
+   * this.startsWith(str, k)
+   * }</pre>
+   * If no such value of {@code k} exists, then {@code -1} is returned.
+   *
+   * @param   str   the substring to search for.
+   * @return  the index of the first occurrence of the specified substring,
+   *          or {@code -1} if there is no such occurrence.
+   */
+  default Optional<CodePointIndex> codePointIndexOf(String str) {
+    int indexOf = indexOf(str);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointIndexOf(Source str) {
+    return codePointIndexOf(str.sourceAsString());
+  }
+
+  /**
+   * Returns the index within this string of the last occurrence of the
+   * specified substring.  The last occurrence of the empty string ""
+   * is considered to occur at the index value {@code this.length()}.
+   *
+   * <p>The returned index is the largest value {@code k} for which:
+   * <pre>{@code
+   * this.startsWith(str, k)
+   * }</pre>
+   * If no such value of {@code k} exists, then {@code -1} is returned.
+   *
+   * @param   str   the substring to search for.
+   * @return  the index of the last occurrence of the specified substring,
+   *          or {@code -1} if there is no such occurrence.
+   */
+  default Optional<CodePointIndex> codePointLastIndexOf(String str) {
+    int indexOf = lastIndexOf(str);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointLastIndexOf(Source str) {
+    return codePointLastIndexOf(str.sourceAsString());
+  }
+  
+  /**
+   * Returns the index within this string of the last occurrence of the
+   * specified substring, searching backward starting at the specified index.
+   *
+   * <p>The returned index is the largest value {@code k} for which:
+   * <pre>{@code
+   *     k <= Math.min(fromIndex, this.length()) &&
+   *                   this.startsWith(str, k)
+   * }</pre>
+   * If no such value of {@code k} exists, then {@code -1} is returned.
+   *
+   * @param   str         the substring to search for.
+   * @param   fromIndex   the index to start the search from.
+   * @return  the index of the last occurrence of the specified substring,
+   *          searching backward from the specified index,
+   *          or {@code -1} if there is no such occurrence.
+   */
+  default Optional<CodePointIndex> codePointLastIndexOf(String str, int fromIndex) {
+    int indexOf = lastIndexOf(str,fromIndex);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointLastIndexOf(String str, CodePointIndex fromIndex) {
+    return codePointLastIndexOf(str,fromIndex.value());
+  }
+  
+  default Optional<CodePointIndex> codePointLastIndexOf(Source str, CodePointIndex fromIndex) {
+    return codePointLastIndexOf(str.sourceAsString(),fromIndex.value());
+  }
+
+  
+  /**
+   * Returns the index within this string of the first occurrence of the
+   * specified substring, starting at the specified index.
+   *
+   * <p>The returned index is the smallest value {@code k} for which:
+   * <pre>{@code
+   *     k >= Math.min(fromIndex, this.length()) &&
+   *                   this.startsWith(str, k)
+   * }</pre>
+   * If no such value of {@code k} exists, then {@code -1} is returned.
+   *
+   * @param   str         the substring to search for.
+   * @param   fromIndex   the index from which to start the search.
+   * @return  the index of the first occurrence of the specified substring,
+   *          starting at the specified index,
+   *          or {@code -1} if there is no such occurrence.
+   */
+  default Optional<CodePointIndex> codePointIndexOf(String str, int fromIndex) {
+    int indexOf = indexOf(str,fromIndex);
+    if(indexOf < 0) {
+      return Optional.empty();
+    }
+    return Optional.of(toCodePointIndex(new StringIndex(indexOf)));
+  }
+  
+  default Optional<CodePointIndex> codePointIndexOf(String str, CodePointIndex fromIndex) {
+    return codePointIndexOf(str,fromIndex.value());
+  }
+  
+  default Optional<CodePointIndex> codePointIndexOf(Source str, CodePointIndex fromIndex) {
+    return codePointIndexOf(str.sourceAsString(),fromIndex.value());
+  }
+
 }
